e81611fc3e50d44ea85c03c2ac9711618f01d7cf
Nicole Tam <nicoletamtv@gmail.com>
2023-07-05 01:31:00 +1000

    123

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const unpack = (data, key) => data.map(row => row[key]);
 
-/* pie chart to show world palm oil production vs indonesia's production. year is unneccessary to unpack but nice to have next to all the data */
+/* pie chart to show world palm oil production vs indonesia's production. year is unneccessary to unpack but nice to have next to all the data if changed in the future */
 Plotly.d3.csv("data/productionPieChart.csv", production_data => {
   const region = unpack(production_data, 'Entity');
   const year = unpack(production_data, 'Year');
@@ -18,12 +18,18 @@ Plotly.d3.csv("data/productionPieChart.csv", production_data => {
   ];
 
   var layout = {
-    title: {
-      text: 'Palm Oil Production (2020)'
-    }
+      height:500,
+      title: {
+      text: 'Palm Oil Production (2020)',
+    },
+    paper_bgcolor: 'rgba(0, 0, 0, 0)',  // transparent background and plot
+    plot_bgcolor: 'rgba(0, 0, 0, 0)'
   }
 
-  Plotly.newPlot('production-pie-chart', data, layout);
+  var config = {responsive: true}
+
+
+  Plotly.newPlot('production-pie-chart', data, layout,config);
 });
 
 
@@ -54,8 +60,8 @@ Plotly.d3.csv("data/oil-yield-by-crop-2020.csv", yield_data => {
 });
 
 Plotly.d3.csv("data/land-use-for-vegetable-oil-crops-cleaned.csv", harvested_data => {
-  const oilHarvest = harvested_data.map(data => data['Oil Type']);
-  const areaHarvest = harvested_data.map(data => data['Area Harvested (hectares)']);
+  const oilHarvest = unpack(harvested_data,'Oil Type');
+  const areaHarvest = unpack(harvested_data,'Area Harvested (hectares)');
 
   var data = [
     {
@@ -69,64 +75,28 @@ Plotly.d3.csv("data/land-use-for-vegetable-oil-crops-cleaned.csv", harvested_dat
   var layout = {
     title: 'Land Use for Vegetable Oil',
     treemapcolorway: ["green,orange"],
-    autosize: true
+    autosize: true,
   };
 
   Plotly.newPlot('harvest-treemap', data, layout);
 });
 
-Plotly.d3.csv("data/exported-deforestation.csv", deforestation_data => {
-  const location = unpack(deforestation_data, 'Code');
-  const exportedDeforestation = unpack(deforestation_data, 'exported_deforestation');
-  const country = unpack(deforestation_data, 'Entity');
-
-  var data = [{
-    type: 'choropleth',
-    locations: location,
-    z: exportedDeforestation,
-    text: country,
-    colorscale: 'Jet',
-    marker: {
-      line: {
-        color: 'rgb(180,180,180)',
-        width: 0.5
-      }
-    },
-    colorbar: {
-      ticksuffix: '%',
-      title: 'Global Exported Deforestation'
-    }
-  }];
-
-  var layout = {
-    width: 1600,
-    height: 800,
-    title: 'Global Exported Deforestation',
-    geo: {
-      showframe: false,
-    }
-  };
-
-  Plotly.newPlot("deforestation-map", data, layout);
-});
-
-
-Plotly.d3.csv("data/exported-deforestation.csv", function(err, rows){
-
-  function filter_and_unpack(rows, key, year) {
+function filterAndUnpack(rows, key, year) { // made into global function because two of my chloropleths require it
   return rows.filter(row => row['Year'] == year).map(row => row[key])
   }
+
+Plotly.d3.csv("data/exported-deforestation.csv", function(err, rows){ // adapted from https://plotly.com/javascript/map-animations/
 
   var frames = []
   var slider_steps = []
 
   var n = 8;
   var num = 2005;
-  for (var i = 0; i <= n; i++) {
-    var z = filter_and_unpack(rows, 'exported_deforestation', num)
-    var locations = filter_and_unpack(rows, 'Code', num)
-    var text = filter_and_unpack(rows, 'Entity', num)
-    frames[i] = {data: [{z: z, locations: locations, text: locations}], name: num}
+  for (var i = 0; i <= n; i++) { //because the chloropleths have multiple years, a for loop and vars are needed instead of const.
+    var z = filterAndUnpack(rows, 'exported_deforestation', num)
+    var locations = filterAndUnpack(rows, 'Code', num)
+    var text = filterAndUnpack(rows, 'Entity', num)
+    frames[i] = {data: [{z: z, locations: locations, text: text}], name: num} // changed the text to use entity so it would display the country name when hovered over instead of the code
     slider_steps.push ({
         label: num.toString(),
         method: "animate",
@@ -143,17 +113,24 @@ Plotly.d3.csv("data/exported-deforestation.csv", function(err, rows){
 var data = [{
       type: 'choropleth',
       locationmode: 'world',
+      colorscale: 'Jet',
       locations: frames[0].data[0].locations,
       z: frames[0].data[0].z,
       text: frames[0].data[0].text,
-      zauto: false,
-      zmin: 30,
-      zmax: 90
-
+      zauto: false, //so that it doesn't automatically change between each frame
+      colorbar: {
+        ticksuffix: 'ha',
+        tickformat: ',',  // Add a comma as a thousands separator
+      }
 }];
 
 var layout = {
-    title: 'Global Exported Deforestation',
+    title: {
+      text: 'Exported Deforestation (Hectares)',
+    },
+    width: 900,
+    height: 600,
+    margin: { l: 10, r: 10, t: 80, b: 10 },
     geo:{
        scope: 'world',
        countrycolor: 'rgb(255, 255, 255)',
@@ -164,7 +141,6 @@ var layout = {
        subunitcolor: 'rgb(255, 255, 255)',
        lonaxis: {},
        lataxis: {},
-       showframe: false,
     },
     updatemenus: [{
       x: 0.1,
@@ -215,7 +191,7 @@ var layout = {
       pad: {t: 50, b: 10},
       currentvalue: {
         visible: true,
-        prefix: "Year:",
+        prefix: "Year: ",
         xanchor: "right",
         font: {
           size: 20,
@@ -229,7 +205,134 @@ var layout = {
     }]
 };
 
-Plotly.newPlot('myDiv', data, layout).then(function() {
-    Plotly.addFrames('myDiv', frames);
+Plotly.newPlot('expD', data, layout).then(function() {
+    Plotly.addFrames('expD', frames);
   });
 })
+
+
+Plotly.d3.csv("data/palm-oil-production-chloro.csv", function(err, rows){ // adapted from https://plotly.com/javascript/map-animations/
+
+  var frames = []
+  var slider_steps = []
+
+  var n = 59;
+  var num = 1961;
+  for (var i = 0; i <= n; i++) {
+    var z = filterAndUnpack(rows, 'Palm oil production (tonnes)', num)
+    var locations = filterAndUnpack(rows, 'Code', num)
+    var text = filterAndUnpack(rows, 'Entity', num)
+    frames[i] = {data: [{z: z, locations: locations, text: text}], name: num}
+    slider_steps.push ({
+        label: num.toString(),
+        method: "animate",
+        args: [[num], {
+            mode: "immediate",
+            transition: {duration: 300},
+            frame: {duration: 300}
+          }
+        ]
+      })
+    num = num + 1
+  }
+
+var data = [{
+      type: 'choropleth',
+      locationmode: 'world',
+      colorscale: 'Jet',
+      locations: frames[0].data[0].locations,
+      z: frames[0].data[0].z,
+      text: frames[0].data[0].text,
+      zauto: false, //so that it doesn't automatically change between each frame
+      colorbar: {
+        ticksuffix: 't',
+        tickformat: ',',  // Add a comma as a thousands separator
+      }
+}];
+
+var layout = {
+    title: {
+      text: 'Palm Oil Production (Tonnes)',
+    },
+    width: 900,
+    height: 600,
+    margin: { l: 10, r: 10, t: 80, b: 10 },
+    geo:{
+       scope: 'world',
+       countrycolor: 'rgb(255, 255, 255)',
+       showland: true,
+       landcolor: 'rgb(217, 217, 217)',
+       showlakes: true,
+       lakecolor: 'rgb(255, 255, 255)',
+       subunitcolor: 'rgb(255, 255, 255)',
+       lonaxis: {},
+       lataxis: {},
+    },
+    updatemenus: [{
+      x: 0.1,
+      y: 0,
+      yanchor: "top",
+      xanchor: "right",
+      showactive: false,
+      direction: "left",
+      type: "buttons",
+      pad: {"t": 87, "r": 10},
+      buttons: [{
+        method: "animate",
+        args: [null, {
+          fromcurrent: true,
+          transition: {
+            duration: 100, //sped up because of how much more data there is available for this chloropleth compared to the other
+          },
+          frame: {
+            duration: 100
+          }
+        }],
+        label: "Play"
+      }, {
+        method: "animate",
+        args: [
+          [null],
+          {
+            mode: "immediate",
+            transition: {
+              duration: 0
+            },
+            frame: {
+              duration: 0
+            }
+          }
+        ],
+        label: "Pause"
+      }]
+    }],
+    sliders: [{
+      active: 0,
+      steps: slider_steps,
+      x: 0.1,
+      len: 0.9,
+      xanchor: "left",
+      y: 0,
+      yanchor: "top",
+      pad: {t: 50, b: 10},
+      currentvalue: {
+        visible: true,
+        prefix: "Year: ",
+        xanchor: "right",
+        font: {
+          size: 20,
+          color: "#666"
+        }
+      },
+      transition: {
+        duration: 100,
+        easing: "cubic-in-out"
+      }
+    }]
+};
+
+Plotly.newPlot('production-chloro', data, layout).then(function() {
+    Plotly.addFrames('production-chloro', frames);
+  });
+})
+
